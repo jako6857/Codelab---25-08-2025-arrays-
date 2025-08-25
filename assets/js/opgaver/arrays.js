aa0b37e602176a6506a4409ab158ace638b918bd
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-08-25 12:54:38 +0200

    opgave 1begge grønne

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -1,12 +1,16 @@
 /* arrays */
 
 //Opgave 1
-const myArray =/*din array her*/;
-
+const myArray = ['element 1', 'element 2', 'element 3', 'element 4', 'element 5'];
+console.log(myArray);
 
 
 
 // opgave 2 console.logs
 
-
+console.log(myArray[2]);
+console.log(myArray[4]);
+console.log(myArray[0]);
+console.log(myArray[3]);
+console.log(myArray[1]);
 
